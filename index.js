6abc5d65d68e3983913304cc5ae7f3af850eeb97
Hasib787 <mr.hasib45@gmail.com>
2021-04-01 02:20:15 +0600

    connect database and create post enpoint

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express')
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config()
 
-const MongoClient = require('mongodb').MongoClient;
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express()
-
-console.log(process.env.DB_USER)
+app.use(bodyParser.json());
+app.use(cors());
 
 client.connect(err => {
-  const collection = client.db("emaJohnStore").collection("products");
- console.log('database connected')
+  const products = client.db("emaJohnStore").collection("products");
+    
+    app.post('/addProduct', (req, res) => {
+        const product = req.body;
+        products.insertOne(product)
+        .then(result => {
+            console.log(result);
+        })
+    })
 });
 
 app.get('/', (req, res) => {
