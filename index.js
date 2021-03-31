3c8ee5a072bc4385f27d4a02e942d03490ff1182
Hasib787 <mr.hasib45@gmail.com>
2021-04-01 01:50:15 +0600

    connected to the database

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-d
+const express = require('express')
+require('dotenv').config()
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fg2cz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const app = express()
+
+console.log(process.env.DB_USER)
+
+client.connect(err => {
+  const collection = client.db("emaJohnStore").collection("products");
+ console.log('database connected')
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(5000)
